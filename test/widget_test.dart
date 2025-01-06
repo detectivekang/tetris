3b16dada523e6aa7b8ce774c622d51e77ae9de91
detectivekang <kangseabich@naver.com>
2025-01-06 23:00:42 +0900

    Update web content3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tetris/main.dart';
+import 'package:tetris_game/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
